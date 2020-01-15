ca10d4093b1456bd0a09d56b13ff7e0df07deee3
Aaron <aarongillies88@gmail.com>
2020-01-15 17:58:56 -0500

    Set up server and begun working on user CRUD

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,24 +2,110 @@ const express = require('express');
 
 const router = express.Router();
 
+const users = require('./userDb');
+
+const posts = require('../posts/postDb');
+
+// POST -- Add user to the user database
+
 router.post('/', (req, res) => {
-  // do your magic!
+  users.insert(req.body)
+  .then(user => {
+    res.status(201).json(user)
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      errorMessage: 'This user could not be saved to the database'
+    });
+  });
 });
+
+// POST - Add a post to a user specified by the ID passed in the request body
 
 router.post('/:id/posts', (req, res) => {
-  // do your magic!
+  const postInfo = req.body;
+  const {text, user_id} = postInfo
+  posts.insert(postInfo)
+  .then(post => {
+    if (!user_id) {
+      res.status(404).json({
+        errorMessage: 'The user with the specified ID does not exist.'
+      })
+    }
+    else if (!text) {
+      res.status(400).json({
+        errorMessage: 'Please provide text for the comment.'
+      })
+    }
+    else if (text && user_id) {
+      res.status(201).json(post)
+    }
+    else {
+      res.status(500).json({
+        errorMessage: 'There was an error while saving the comment to the database'
+      })
+    }
+  });
 });
+
+// GET - Get a list of users from the user database returned as an array
 
 router.get('/', (req, res) => {
-  // do your magic!
+  users.get()
+  .then(user => {
+    res.status(200).json(user)
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      errorMessage: 'The users could not be retrieved.'
+    });
+  });
 });
+
+// GET - Get a user 
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  const id = req.params.id
+  users.getById(id)
+  .then(user => {
+    if (!id) {
+      res.status(404).json({
+        errorMessage: 'The user with the specified ID does not exist.'
+      })
+    }
+    else if (id) {
+      res.status(200).json(user)
+    }
+    else {
+      res.status(500).json({
+        errorMessage: 'The user information could not be retrieved'
+      })
+    }
+  });
 });
 
+// GET - Get a post by the specified ID passed into the request body.
+
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  const id = req.params.id
+  posts.getById(id) 
+  .then(post => {
+    if (!id) {
+      res.status(404).json({
+        errorMessage: 'The post with the specified ID does not exist.'
+      })
+    }
+    else if (id) {
+      res.status(200).json(post)
+    }
+    else {
+      res.status(500).json({
+        errorMessage: 'The post information could not be retrieved.'
+      })
+    }
+  });
 });
 
 router.delete('/:id', (req, res) => {
